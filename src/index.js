db095211a714535c0b648aafcec860bac9eaf260
Fernando Figueiredo <fernandof@interline.com>
2023-02-24 13:33:23 -0700

    Completed d022423-index-setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
+// import dependencies and libraries
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import Router from './router';
 
+// import bootstrap as well as css documents to build frontend
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './styles/app.css';
+
+// import component and library to support store
+import { Provider } from 'react-redux'; // import Provider to work with the store
+import { store } from './store' //import store to the index of the App and make it accessible (wrap) to all the components
+
+// basic setup to create the root element
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Provider store={store}>
+        <Router/>
+    </Provider>
+);
